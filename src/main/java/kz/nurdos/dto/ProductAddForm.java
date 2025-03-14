6e887563d430169f4082e1f066473f9744d9b4d9
Nurdos Ramazan <nurdos.ramazan@gmail.com>
2025-03-14 11:30:43 +0500

    new dtos

--- src/main/java/kz/nurdos/dto/ProductAddForm.java
+++ src/main/java/kz/nurdos/dto/ProductAddForm.java
@@ -1,23 +1,29 @@
 package kz.nurdos.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
+//TODO: nonrelated fields in json request are accepted(jsonignore?)
 @Getter
 public class ProductCreateForm {
+    @NotNull(message = "Name must be specified")
     @Length(min = 2, max = 50, message = "Name length is unacceptable.")
     private String name;
 
     @Length(max = 500, message = "Maximum description text exceeded")
     private String description;
 
+    @NotNull(message = "Price must be specified")
     @PositiveOrZero(message = "Price is unacceptable")
     private Double price;
 
+    @NotNull(message = "Category must be specified")
     @Length(max = 30, message = "Category length is unacceptable")
     private String category;
 
+    @NotNull(message = "Quantity must be specified")
     @PositiveOrZero(message = "Quantity is unacceptable")
     private Integer quantity;
 }
---
+++ src/main/java/kz/nurdos/dto/ProductUpdateInfoForm.java
@@ -0,0 +1,8 @@
+package kz.nurdos.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProductUpdateForm {
+    
+}
